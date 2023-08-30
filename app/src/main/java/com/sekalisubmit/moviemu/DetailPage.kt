907cee5887f3456220d7387fd3047a2cf1212d98
ags <71609913+letdummy@users.noreply.github.com>
2023-08-30 23:53:12 +0700

    initial commit, adding resource needed

---
+++ app/src/main/java/com/sekalisubmit/moviemu/DetailPage.kt
@@ -0,0 +1,11 @@
+package com.sekalisubmit.moviemu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_page)
+    }
+}
